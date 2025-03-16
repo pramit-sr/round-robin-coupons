e9bca1d1705798c281c5015b4bbe2b11564c7891
pramit_sr <mepramitsarkarr@gmail.com>
2025-03-16 13:11:17 +0530

    small changes

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import { checkCookieAbuse } from "./cookieTracking.js";  // Cookie tracking
 const app = express();
 const PORT = 5001;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://round-robin-coupons-lyart.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
